13066a78dacd1bcc2ca5461d117d57f39f51d604
GDJ <GDJ@DESKTOP-ECFFCGV>
2024-04-18 16:45:23 +0900

    sm_충돌실험

--- semitest/src/semitest/test/semitest/Test.java
+++ semitest/src/semitest/test/semitest/Test.java
@@ -1,12 +1,30 @@
 package semitest.test.semitest;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class Test {
-	private int age;
-	private String name;
 	
-	private String juwhanInform() {
-		return "솔민 바보^^";
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	public void checkConnection() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String path = Test.class.getResource("./").toString();
+		try {
+			FileReader fr = new FileReader(path);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
 	
 	
 	

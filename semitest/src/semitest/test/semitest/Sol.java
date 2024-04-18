2eb1fdebf15c9187c888c1eed1833f80bbfd329f
LG <LG@DESKTOP-AFLOV2G>
2024-04-18 16:32:03 +0900

    ju_sol 추가

---
+++ semitest/src/semitest/test/semitest/Sol.java
@@ -0,0 +1,43 @@
+package semitest.test.semitest;
+
+public class Sol {
+	private String name = "김솔민";
+	private int age = 25;
+	private double height = 193.4;
+	
+	public Sol() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public Sol(String name, int age, double height) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+	
+}
